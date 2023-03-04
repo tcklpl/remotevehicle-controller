6c0b227b101e2fa215166ec9a60d71775113d928
tcklpl <negroniluan@gmail.com>
2023-03-04 11:42:01 -0300

    Comments on the example

--- src/me/negroni/remotevehicle/controller/example/ExampleApplicationGUI.java
+++ src/me/negroni/remotevehicle/controller/example/ExampleApplicationGUI.java
@@ -23,8 +23,12 @@ public class ExampleApplicationGUI {
     private final RemoteVehicle remoteVehicle;
 
     public ExampleApplicationGUI() {
+        // First of all we need an instance of RemoveVehicle
         remoteVehicle = new RemoteVehicle();
+        // Function below, register callbacks for connecting and disconnecting
         register();
+
+        // GUI Window Initialization
         JFrame frame = new JFrame("test");
         frame.setContentPane(mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -35,13 +39,19 @@ public class ExampleApplicationGUI {
         btnRequestImg.setEnabled(false);
         frame.setVisible(true);
 
+        // Add all available resolutions to a GUI combo box
         Arrays.stream(CameraImageSize.values()).forEach(x -> cmbRes.addItem(x));
 
+        // When the user presses the 'Request Image' button we'll invoke VehicleCamera#requestCameraImage, this
+        // function will require a consumer for an array of bytes (byte[]).
         btnRequestImg.addActionListener(e -> remoteVehicle.getCamera().requestCameraImage(b -> {
             imgLabel.setIcon(new ImageIcon(b));
             btnRequestImg.setEnabled(true);
         }));
 
+        // When the user presses the 'Change Resolution' button we'll try to change the camera resolution through
+        // VehicleCamera#requestResolutionChange. This function required the desired resolution and two callbacks:
+        // the first one for success and the second one if the operation fails.
         btnRes.addActionListener(e -> {
             lblRes.setText("Resolução: (Mudando)");
             remoteVehicle.getCamera().requestResolutionChange((CameraImageSize) cmbRes.getSelectedItem(),
@@ -49,11 +59,14 @@ public class ExampleApplicationGUI {
                     () -> lblRes.setText("Resolução: (ERRO)"));
         });
 
+        // As the vehicle is constantly sending a connection package on the network, we only need to allow the
+        // controller to respond to it and start the connection sequence.
         btnConnect.addActionListener(e -> {
             remoteVehicle.getCommunication().setShouldTryToConnect(true);
             btnConnect.setEnabled(false);
         });
 
+        // Disconnect from the vehicle
         btnDisconnect.addActionListener(e -> {
             remoteVehicle.getCommunication().disconnect();
             btnDisconnect.setEnabled(false);
@@ -61,12 +74,14 @@ public class ExampleApplicationGUI {
     }
 
     private void register() {
+        // Update title and button status when the controller connects to the vehicle
         remoteVehicle.getCommunication().registerCustomCallback(PacketType.PACKET_ACCEPTED_CONNECTION, c -> {
             titleLabel.setText("Status: CONECTADO (" + c.getRemoteAddress().toString() + ")");
             btnDisconnect.setEnabled(true);
             btnRequestImg.setEnabled(true);
         });
 
+        // Update title and button status when the controller disconnects from the vehicle
         remoteVehicle.getCommunication().registerCustomCallback(PacketType.PACKET_CONFIRM_CONNECTION_END, c -> {
             titleLabel.setText("Status: NÃO CONECTADO");
             btnConnect.setEnabled(true);
